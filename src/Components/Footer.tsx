dd00d5f7cb82482afe42fadf4315a8223ccc7f67
silvershade1 <silvershade1000@gmail.com>
2024-10-31 21:18:23 +0330

    style : update footer style

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,34 +1,26 @@
 function Footer() {
   return (
-    <footer className="bg-gray-800 py-4">
+    <footer className="bg-white py-4">
       <div className="max-w-screen-2xl mx-auto px-8 flex flex-col md:flex-row items-center justify-between">
-        <p className="text-gray-300 text-center md:text-left">
-          © {new Date().getFullYear()} Your Company Name. All rights reserved.
+        <p className="text-gray-800 text-center md:text-left">
+          © {new Date().getFullYear()} m-mohammad-d . All rights reserved.
         </p>
         <div className="flex space-x-4 mt-2 md:mt-0">
           <a
-            href="https://github.com/your-profile"
+            href="https://github.com/m-mohammad-d"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-gray-300 hover:text-blue-500"
+            className="text-gray-800 hover:text-blue-500"
           >
             GitHub
           </a>
           <a
-            href="https://twitter.com/your-profile"
+            href="https://t.me/silver_shade1"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-gray-300 hover:text-blue-500"
+            className="text-gray-800 hover:text-blue-500"
           >
-            Twitter
-          </a>
-          <a
-            href="https://linkedin.com/in/your-profile"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-300 hover:text-blue-500"
-          >
-            LinkedIn
+            telegram
           </a>
         </div>
       </div>
